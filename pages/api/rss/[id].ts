8d9b681aaa6345c9953e629180e0a31e8288ed85
wasuken <wevorence@gmail.com>
2022-12-20 01:28:39 +0900

    RSSURLを元に再取得する処理追加

---
+++ pages/api/rss/[id].ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { saveRSSItemsFromId } from '../../../lib/rss'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let { id } = req.query
+  id = parseInt(id);
+  if (req.method === "PUT") {
+    await saveRSSItemsFromId(id);
+    // RSS登録
+    res.status(200).json({ msg: "success" });
+  }
+  return;
+}

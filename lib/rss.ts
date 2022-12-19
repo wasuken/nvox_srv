297351e052340718b236a4caa27d0025f9659007
wasuken <wevorence@gmail.com>
2022-12-19 09:32:16 +0900

    バッチ処理実装。結構実行時間長い

--- lib/rss.ts
+++ lib/rss.ts
@@ -1,3 +1,8 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+const prisma = new PrismaClient();
+import fs from "fs";
+import fetch from "node-fetch";
+
 export async function fetchRSS(rss_url) {
   const Parser = require("rss-parser");
   const parser = new Parser();
@@ -6,8 +11,49 @@ export async function fetchRSS(rss_url) {
   return { items: feed.items, title: feed.title };
 }
 
+function genUUID() {
+  return "xxxxxxxx_xxxx_4xxx_yxxx_xxxxxxxxxxxx".replace(/[xy]/g, function (a) {
+    let r = (new Date().getTime() + Math.random() * 16) % 16 | 0,
+      v = a == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+async function createVoice(text) {
+  const res = await fetch(
+    `http://voicevox_engine:50021/audio_query?text=${text}&speaker=0`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const query = await res.json();
+
+  const sound_row = await fetch(
+    `http://voicevox_engine:50021/synthesis?speaker=0&enable_interrogative_upspeak=true`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "audio/wav",
+        responseType: "stream",
+      },
+      body: JSON.stringify(query),
+    }
+  );
+
+  const voice_id = genUUID();
+  const path = `data/wav/${voice_id}.wav`;
+  const dest = fs.createWriteStream(path);
+  sound_row.body.pipe(dest);
+  return path;
+}
+
 // バッチ処理とかで少しずつ消化していく
-export async function updateRSSItemVoices(n: int){
+export async function updateRSSItemVoices(n: int) {
   const not_dl_recs = await prisma.rSSItem.findMany({
     take: n,
     where: {
@@ -16,19 +62,23 @@ export async function updateRSSItemVoices(n: int){
     orderBy: {
       createdAt: "asc",
     },
-  })
+  });
+  console.log("count", not_dl_recs.length);
   // どちゃくそ遅そう
-  Promise.All(not_dl_recs.map(async (rec) => {
-    const text = rec.desc;
-    const path = await createVoice(text);
-    await prisma.rSSItem.update({
-      where: {
-        id: rec.id,
-      },
-      data: {
-        voice_filepath: path,
-        voice_downloaded: true,
-      },
+  await Promise.all(
+    not_dl_recs.map(async (rec) => {
+      const text = rec.desc;
+      const path = await createVoice(text);
+      await prisma.rSSItem.update({
+        where: {
+          id: rec.id,
+        },
+        data: {
+          voice_filepath: path,
+          voice_downloaded: true,
+        },
+      });
     })
-  }));
+  );
+  console.log("update", "finish");
 }
---
+++ pages/api/rss/[n].ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { updateRSSItemVoices } from '../../../lib/rss'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let { n } = req.query
+  n = parseInt(n);
+  if (req.method === "POST") {
+    if(n > 10) {
+      res.status(400).json({ msg: "invalid n, too many" });
+      return;
+    }
+    await updateRSSItemVoices(n);
+    // RSS登録
+    res.status(200).json({ msg: "success" });
+  }
+  return;
+}
